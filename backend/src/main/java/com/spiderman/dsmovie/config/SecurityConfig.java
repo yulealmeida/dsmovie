7fb9eb18dce07fc97075b1f7eed101ed3c700f23
Yule Almeida <yule.alme2@gmail.com>
2024-07-22 13:50:08 -0300

    Domain model, database seed

--- backend/src/main/java/com/spiderman/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/spiderman/dsmovie/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.spiderman.dsmovie;
+package com.spiderman.dsmovie.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
